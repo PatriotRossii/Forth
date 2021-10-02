f8eba130afeaa79fcf0408fa2c365daf19be19f1
PatriotRossii <patriotrossii2019@mail.ru>
2021-10-02 22:51:14 +0500

    Implement some basic shit, move state to public to keep it simple for beginning

--- Interpreter.h
+++ Interpreter.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <istream>
+
+#include <vector>
 #include <stack>
 
 #include "Value.h"
 
 class Interpreter
 {
-	std::stack<Value> state;
+	std::vector<std::string> lines;
 public:
+	std::stack<Value> state;
+
 	void load(std::istream& stream);
 	void clear();
 	void run();
